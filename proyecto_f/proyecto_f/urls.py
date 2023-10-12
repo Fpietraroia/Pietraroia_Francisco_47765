b8d986e3687c9f37b919293c617f3a1f94718d55
Fpietraroia <fpietraroia@gmail.com>
2023-10-12 18:35:26 -0300

    updated

--- proyecto_f/proyecto_f/urls.py
+++ proyecto_f/proyecto_f/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render, redirect
+
+def redireccion_inicio(request):
+    return redirect('/MedicalDB/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', include('medicalapp.urls')),
-    ]
+    path('MedicalDB/', include('medicalapp.urls')),
+    path('', redireccion_inicio, name='redireccion-inicio'),
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
